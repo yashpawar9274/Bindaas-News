bae1a0996bad864c6c9067b4b27dda5bb7c2f31a
Yash Pawar <yash.pawar@theemcoe.org>
2025-08-11 16:16:37 +0530

    Upgraded project with latest changes

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-const NotFound = () => {
+export default function Custom404() {
   const location = useLocation();
 
   useEffect(() => {
@@ -14,14 +14,14 @@ const NotFound = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="text-xl text-gray-600 mb-4">
+          Oops! The page you are looking for does not exist.
+        </p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
         </a>
       </div>
     </div>
   );
-};
-
-export default NotFound;
+}
